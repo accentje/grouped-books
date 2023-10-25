c27405318fa9f55915d634f5e4df86e12c8aa068
Jenisi Armamento <jenisi.armamento@accenture.com>
2023-10-25 15:17:52 +0800

    Sort alphabetically returned array; remove console.logs

--- client/grouped-books/src/App.tsx
+++ client/grouped-books/src/App.tsx
@@ -19,7 +19,7 @@ enum ActionKind {
   UpdateMaleHeader,
   UpdateFemaleHeader,
   MaleAuthoredBooks,
-  FemaleAuthoredBooks
+  FemaleAuthoredBooks,
 }
 
 type StateType = {
@@ -28,113 +28,173 @@ type StateType = {
   femaleHeader?: string;
   maleAuthoredBooks?: string[];
   femaleAuthoredBooks?: string[];
-}
+};
 
 type ActionType = {
   type: ActionKind;
   booksPayload?: string[];
   headerPayload?: string;
   authorsPayload?: AuthorInfo[];
-}
+};
 
 function reducer(state: StateType, action: ActionType): StateType {
   const { type, headerPayload, booksPayload, authorsPayload } = action;
 
-  switch(type) {
+  switch (type) {
     case ActionKind.UpdateMaleHeader:
-      return { ...state, maleHeader: headerPayload }
+      return { ...state, maleHeader: headerPayload };
     case ActionKind.UpdateFemaleHeader:
-      return { ...state, femaleHeader: headerPayload }
+      return { ...state, femaleHeader: headerPayload };
     case ActionKind.MaleAuthoredBooks:
-      return { ...state, maleAuthoredBooks: booksPayload }
+      return { ...state, maleAuthoredBooks: booksPayload };
     case ActionKind.FemaleAuthoredBooks:
-      return { ...state, femaleAuthoredBooks: booksPayload }
+      return { ...state, femaleAuthoredBooks: booksPayload };
     case ActionKind.StoreAuthors:
-      return { ...state, authorList: authorsPayload }
+      return { ...state, authorList: authorsPayload };
   }
 }
 
 function App() {
   const [state, dispatch] = useReducer(reducer, {
-    authorList: [], 
-    maleHeader: "Books with Male Owners", 
+    authorList: [],
+    maleHeader: "Books with Male Owners",
     femaleHeader: "Books with Female Owners",
     maleAuthoredBooks: [] as string[],
-    femaleAuthoredBooks: [] as string[]
-  } as StateType)
+    femaleAuthoredBooks: [] as string[],
+  } as StateType);
 
   useEffect(() => {
     (async function GetAuthorListAsync() {
-      const authors = await (await fetch("http://localhost:8764/authors")).json();
-      console.log("Authors", authors);
-      console.log(authors.List)
-      
-      dispatch({ type: ActionKind.StoreAuthors, authorsPayload: authors.List })
-      dispatch({ type: ActionKind.MaleAuthoredBooks, booksPayload: GetAuthorListByGender("Male", authors.List) })
-      dispatch({ type: ActionKind.FemaleAuthoredBooks, booksPayload: GetAuthorListByGender("Female", authors.List) })
-    })()
-    
-  }, [])
+      const authors = await (
+        await fetch("http://localhost:8764/authors")
+      ).json();
 
+      dispatch({ type: ActionKind.StoreAuthors, authorsPayload: authors.List });
+      dispatch({
+        type: ActionKind.MaleAuthoredBooks,
+        booksPayload: GetAuthorListByGender("Male", authors.List),
+      });
+      dispatch({
+        type: ActionKind.FemaleAuthoredBooks,
+        booksPayload: GetAuthorListByGender("Female", authors.List),
+      });
+    })();
+  }, []);
 
-  function GetAuthorListByGender(gender: string, authors: AuthorInfo[], isForHardCover = false) {
+  function GetAuthorListByGender(
+    gender: string,
+    authors: AuthorInfo[],
+    isForHardCover = false
+  ) {
     const hardcoverBookType = "Hardcover";
     let filteredBooks: string[] = [];
-    
-    const filteredAuthorsByGender = authors.filter(author => author.gender === gender);
-    console.log(`Filtered by ${gender}`, filteredAuthorsByGender)
-    
-    const authorsWithBooks = filteredAuthorsByGender.filter(x => x.books != null);
-    console.log(authorsWithBooks)
-    
-  if (isForHardCover) {
-    let hardcoverBooks: BookInfo[] = [];
-    for (const author of authorsWithBooks) {
-      hardcoverBooks.push(...author.books.filter(x => x.type === hardcoverBookType))
+
+    const filteredAuthorsByGender = authors.filter(
+      (author) => author.gender === gender
+    );
+
+    const authorsWithBooks = filteredAuthorsByGender.filter(
+      (x) => x.books != null
+    );
+
+    if (isForHardCover) {
+      let hardcoverBooks: BookInfo[] = [];
+      for (const author of authorsWithBooks) {
+        hardcoverBooks.push(
+          ...author.books.filter((x) => x.type === hardcoverBookType)
+        );
+      }
+
+      hardcoverBooks.forEach((book) => filteredBooks.push(book.name));
+
+      return filteredBooks.sort();
     }
-    console.log(state.authorList)
-    console.log("Hardcovers: ", hardcoverBooks)
-    hardcoverBooks.forEach(book => filteredBooks.push(book.name))
-    console.log(`Filtered Hardcover books: `, filteredBooks)
-    return filteredBooks
+
+    for (const author of authorsWithBooks) {
+      author.books.forEach((book) => filteredBooks.push(book.name));
+    }
+
+    return filteredBooks.sort();
   }
 
-  for (const author of authorsWithBooks) {
-    author.books.forEach(book => filteredBooks.push(book.name))
-  }
-
-  console.log("Filtered books: ", filteredBooks)
-  return filteredBooks;
-}
-
-console.log(state)
-
-return (
-  <section>
+  return (
+    <section>
       <nav>
         <header>Owner and Books</header>
       </nav>
       <div className="box-wrapper">
         <div className="box-container">
           <div className="cards-container">
-            <BookCard cardHeader={state.maleHeader} authorList={state.maleAuthoredBooks} />
-            <BookCard cardHeader={state.femaleHeader} authorList={state.femaleAuthoredBooks} />
+            <BookCard
+              cardHeader={state.maleHeader}
+              authorList={state.maleAuthoredBooks}
+            />
+            <BookCard
+              cardHeader={state.femaleHeader}
+              authorList={state.femaleAuthoredBooks}
+            />
           </div>
           <div className="buttons-wrapper">
             <hr />
             <div className="buttons-container">
-              <button onClick={() => {
-                dispatch({ type: ActionKind.UpdateMaleHeader, headerPayload: "Hardcover Books with Male Owners" })
-                dispatch({ type: ActionKind.UpdateFemaleHeader, headerPayload: "Hardcover Books with Female Owners" })
-                dispatch({ type: ActionKind.MaleAuthoredBooks, booksPayload: GetAuthorListByGender("Male", state.authorList as AuthorInfo[], true) })
-                dispatch({ type: ActionKind.FemaleAuthoredBooks, booksPayload: GetAuthorListByGender("Female", state.authorList as AuthorInfo[], true) })
-              }}>Hardcover only</button>
-              <button onClick={() => {
-                dispatch({ type: ActionKind.UpdateMaleHeader, headerPayload: "Books with Male Owners" })
-                dispatch({ type: ActionKind.UpdateFemaleHeader, headerPayload: "Books with Female Owners" })
-                dispatch({ type: ActionKind.MaleAuthoredBooks, booksPayload: GetAuthorListByGender("Male", state.authorList as AuthorInfo[]) })
-                dispatch({ type: ActionKind.FemaleAuthoredBooks, booksPayload: GetAuthorListByGender("Female", state.authorList as AuthorInfo[]) })
-              }}>Get Books</button>
+              <button
+                onClick={() => {
+                  dispatch({
+                    type: ActionKind.UpdateMaleHeader,
+                    headerPayload: "Hardcover Books with Male Owners",
+                  });
+                  dispatch({
+                    type: ActionKind.UpdateFemaleHeader,
+                    headerPayload: "Hardcover Books with Female Owners",
+                  });
+                  dispatch({
+                    type: ActionKind.MaleAuthoredBooks,
+                    booksPayload: GetAuthorListByGender(
+                      "Male",
+                      state.authorList as AuthorInfo[],
+                      true
+                    ),
+                  });
+                  dispatch({
+                    type: ActionKind.FemaleAuthoredBooks,
+                    booksPayload: GetAuthorListByGender(
+                      "Female",
+                      state.authorList as AuthorInfo[],
+                      true
+                    ),
+                  });
+                }}
+              >
+                Hardcover only
+              </button>
+              <button
+                onClick={() => {
+                  dispatch({
+                    type: ActionKind.UpdateMaleHeader,
+                    headerPayload: "Books with Male Owners",
+                  });
+                  dispatch({
+                    type: ActionKind.UpdateFemaleHeader,
+                    headerPayload: "Books with Female Owners",
+                  });
+                  dispatch({
+                    type: ActionKind.MaleAuthoredBooks,
+                    booksPayload: GetAuthorListByGender(
+                      "Male",
+                      state.authorList as AuthorInfo[]
+                    ),
+                  });
+                  dispatch({
+                    type: ActionKind.FemaleAuthoredBooks,
+                    booksPayload: GetAuthorListByGender(
+                      "Female",
+                      state.authorList as AuthorInfo[]
+                    ),
+                  });
+                }}
+              >
+                Get Books
+              </button>
             </div>
           </div>
         </div>
